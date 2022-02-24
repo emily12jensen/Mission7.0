bc57f45d82abfb758c6eed8128d50135f3ad563e
emily12jensen <90439014+emily12jensen@users.noreply.github.com>
2022-02-24 01:12:12 -0700

    bootstrap

--- Models/Infrastructure/PaginationTagHelper.cs
+++ Models/Infrastructure/PaginationTagHelper.cs
@@ -28,6 +28,10 @@ namespace Mission7._0.Models.Infrastructure
         //different than the View Context
         public PageInfo PageBlah { get; set; }
         public string PageAction { get; set; }
+        public string PageClass { get; set; }
+        public bool PageClassEnabled { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
         public override void Process(TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
@@ -38,6 +42,15 @@ namespace Mission7._0.Models.Infrastructure
             {
                 TagBuilder tb = new TagBuilder("a");
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+
+
+                if (PageClassEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageBlah.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+                tb.AddCssClass(PageClass);
+
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
