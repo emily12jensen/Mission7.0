535316e43b06282537b2e52a2a9ff5e7dad23be9
emily12jensen <90439014+emily12jensen@users.noreply.github.com>
2022-03-14 22:54:28 -0600

    Mission10

--- Models/Book.cs
+++ Models/Book.cs
@@ -11,13 +11,21 @@ namespace Mission7._0.Models
         [Key]
         [Required]
         public int BookID { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Author { get; set; }
+        [Required]
         public string Publisher { get; set; }
+        [Required]
         public string ISBN { get; set; }
+        [Required]
         public string Classification { get; set; }
+        [Required]
         public string Category { get; set;  }
+        [Required]
         public int PageCount { get; set; }
+        [Required]
         public int Price { get; set; }
     }
 }
