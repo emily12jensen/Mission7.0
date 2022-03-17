d421dcd0f43e7c742760c7987792d03c95a33452
emily12jensen <90439014+emily12jensen@users.noreply.github.com>
2022-03-17 01:29:10 -0600

    Mission11

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -10,41 +10,43 @@ namespace Mission7._0.Controllers
 {
     public class AccountController : Controller
     {
-        //private UserManager<IdentityUser> userManager;
-        //private SignInManager<IdentityUser> signInManager;
+        private UserManager<IdentityUser> userManager;
+        private SignInManager<IdentityUser> signInManager;
 
-        //public AccountController(UserManager<IdentityUser> um, SignInManager<IdentityUser> sim)
-        //{
-        //    userManager = um;
-        //    signInManager = sim;
-        //}
-        //[HttpGet]
-        //public IActionResult Login (string returnUrl)
-        //{
-        //    return View(new LoginModel { ReturnURL = returnUrl });
-        //}
-        //[HttpPost]
-        //public async Task<IActionResult> Login(LoginModel loginModel)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        IdentityUser user = await UserManager.FindNameByAsync(loginModel.Username);
+        public AccountController(UserManager<IdentityUser> um, SignInManager<IdentityUser> sim)
+        {
+            userManager = um;
+            signInManager = sim;
+        }
+        [HttpGet]
+        public IActionResult Login (string returnUrl)
+        {
+            return View(new LoginModel { ReturnURL = returnUrl });
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login (LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await userManager.FindByNameAsync(loginModel.Username);
 
-        //        if (User != null)
-        //        {
-        //            await signInManager.SignOutAsync();
-        //            if ((await signInManager.PasswordSignInAsync(user, loginModel.Password, false, false)).Succeeded)
-        //            {
-        //                return Redirect(loginModel?.ReturnURL ?? "/admin");
-        //            }
-        //        }
-                
-                
-        //            ModelState.AddModelError("", "Invalid Name or Password");
-        //            return View(loginModel);
-                
+                if (User != null)
+                {
+                    await signInManager.SignOutAsync();
+                    if ((await signInManager.PasswordSignInAsync(user, loginModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnURL ?? "/Admin");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid Name or Password");
+            return View(loginModel);
+        }
+        public async Task<RedirectResult> Logout (string returnUrl = "/")
+        {
+            await signInManager.SignOutAsync();
 
-        //    }
-        //}
+            return Redirect(returnUrl);
+        }
     }
 }
